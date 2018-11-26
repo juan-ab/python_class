00cfaef09344b54d92739a5653b8c14cc542d1f6
juan-ab <42413380+juan-ab@users.noreply.github.com>
2018-11-26 09:43:09 -0500

    Add files via upload

---
+++ quiz3.ino
@@ -0,0 +1,24 @@
+char serialData;
+int pinLed = 13;
+
+
+void setup() {
+  pinMode(pinLed,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available()>0) {
+  serialData = Serial.read();
+  Serial.print(serialData);
+  
+  if(serialData == "opera") {
+    digitalWrite(pinLed,HIGH);
+  } else {
+    if(serialData == '0') {
+      digitalWrite(pinLed, LOW);
+   }
+  }
+ }
+}
